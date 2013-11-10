6093e1c77a319810dff5b52a5d53ec0df20323de
Shohei Aoki <shoaok@gmail.com>
2013-11-10 20:01:52 +0900

    refactored app.js

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ execCmd = function(cmd) {
     return exec(cmd, {timeout: 1000},
         function(error, stdout, stderr) {
             //console.log('stdout: '+(stdout||'none'));
-            //console.log('stderr: '+(stderr||'none'));
         	console.log("execCmd fired to"+cmd);
             if(error !== null) {
                 console.log('exec error: '+error);
@@ -20,20 +19,8 @@ execCmd = function(cmd) {
     )
 };
 
-var express = require("express");
-var app = express();
-var io = require("socket.io").listen(app.listen(3333));
 var serialport = require('serialport');
-
-//express code
-app.get("/",function(req,res){
-	res.sendfile(__dirname + '/index.html');
-    });
-app.use(express.static(__dirname));
-
-// Serial Port
-//var portName = '/dev/tty.usbserial-A6008hxG'; // Mac環境
-var portName = '/dev/tty.usbmodemfa131'; // Mac環境
+var portName = '/dev/tty.usbmodemfa131'; // Mac
 var sp = new serialport.SerialPort(portName, {
 	baudRate: 9600,
 	dataBits: 8,
@@ -43,41 +30,16 @@ var sp = new serialport.SerialPort(portName, {
 	parser: serialport.parsers.readline("\n")   // ※修正：パースの単位を改行で行う
     });
 
-//socket.io code
-//io.sockets.on('connection',function(socket){
-//	socket.emit('connect_ok',{message:'websocket communication ready'});
-
-	// data from Serial port
-	sp.on('data', function(input) {
-		console.log(input);
-		var buffer = new Buffer(input, 'utf8');
-		var jsonData;
-		try {
-		    jsonData = JSON.parse(buffer);
-		    console.log('code: ' + jsonData.cmd);
-		} catch(e) {
-		    // データ受信がおかしい場合無視する
-		    return;
-		}
-		//socket.emit(jsonData.code, { code: jsonData.cmd });    
-	 //execCmd(jsonData.cmd.replace(/\s+/g, ""));
-	 execCmd(commandJSON[jsonData.cmd]);
-	 });
-
-//socket.on('trigger1',function(data){
-//	console.log(data.my);
-		//setInterval(function(){debugFunc()},2000); //for debug
-//});
-
-	function debugFunc(){
-	    var _date = new Date()
-		var _label = _date.getHours() +':'+_date.getMinutes() +':'+ _date.getSeconds();
-	    var _content = Math.random();
-	    if (_content > 0.8){
-		socket.emit('3f_on',{message:'3F is ON',label:_label,content:_content});
-	    } else if(_content < 0.2) {
-		socket.emit('3f_off',{message:'3F is OFF',label:_label,content:_content});
-	    }
+sp.on('data', function(input) {
+	console.log(input);
+	var buffer = new Buffer(input, 'utf8');
+	var jsonData;
+	try {
+	    jsonData = JSON.parse(buffer);
+	    console.log('code: ' + jsonData.cmd);
+	} catch(e) {
+	    return;
 	}
+ execCmd(commandJSON[jsonData.cmd]);
+ });
 
-//    });
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -10,14 +10,16 @@ void setup(){
 char trans[20]; // Array fro sending number(or string)
 
 void loop(){
- for(int i=0;i<sizeof(swPin)/sizeof(int);i++){
+  for(int i=0;i<sizeof(swPin)/sizeof(int);i++){
     if(digitalRead(swPin[i]) == HIGH){
       memset(trans, 0, 20);
       char *json = &trans[0];
       char *message[] = {"\"play\"","\"pause\"","\"stop\"","\"up\"","\"down\""};
       sprintf(json, "{\"cmd\":%s}",message[i]);
       Serial.println(json);
-      delay(50);
+      //delay(50);
+      while(digitalRead(swPin[i]) == HIGH);
     }
   }
 }
+
